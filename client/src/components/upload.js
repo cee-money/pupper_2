4b6bd6b861d9454029cf2c7fb6b4af47d30e4d85
Claudia Falk <claudiafalk0@gmail.com>
2019-08-07 14:15:11 -0600

    put AWS in pages

--- client/src/components/upload.js
+++ client/src/components/upload.js
@@ -1,13 +1,28 @@
-// import React, { Component } from "react";
+// import React from 'react';
 
-// function uploadBtn() {
-//     return (
-//         <div className="row">
-//             <div className="col-12">
-//                 <a className="btn btn-info btn-lg text-white" href="/AWS" id="uploadAWS">Upload</a>
-//             </div>
-//         </div>
-//     )
-// }
 
-// export default uploadBtn;
+// <div className="col-md-4 col-xs-12 form-group App">
+// <label for='upload'>Your Pupper's Photo*:</label>
+// <center>
+//     {this.state.success ? <SuccessMessage /> : null}
+//     {this.state.error ? <ErrorMessage /> : null}
+// <input 
+//     className="form-control" 
+//     id="dog-photo" 
+//     name="image"
+//     value={this.state.image}
+//     onChange={this.handleChange} 
+//     ref={(ref) => { this.uploadInput = ref; }} 
+//     type="file"
+// />
+// </center>
+// </div>
+//  <div className="col-md-2 col-xs-12 form-group">
+//  <button 
+//      className="btn btn-info btn-sm"
+//      onClick={this.handleUpload}
+//      style={uploadBtn}>Upload
+//  </button>
+// </div>
+
+// export default upload;
