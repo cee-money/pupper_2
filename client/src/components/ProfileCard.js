b93f8252199960fe75fc7dd51fab1201617992ae
Charlie Parsons <cdaltonparsons@gmail.com>
2019-08-12 13:26:38 -0600

    holds original values as default when editing

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,40 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import EditRemoveBtn from "../components/EditRemoveBtn";
 import ProfileImage from "../components/ProfileImage";
 import ProfileSummary from "../components/ProfileSummary";
 import ProfileEdit from "../components/ProfileEdit";
 
-
 const h4Style = {
-    float: "left"
+  float: "left"
+};
 
-}
+class ProfileCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // key: this.props.key,
+      _id: this.props._id,
+      url: this.props.url,
+      dogName: this.props.dogName,
+      size: this.props.size,
+      familyFriendly: this.props.familyFriendly,
+      energetic: this.props.energetic,
+      strangerDanger: this.props.strangerDanger,
+      dogDanger: this.props.dogDanger,
+      largeDogDanger: this.props.largeDogDanger,
+      smallDogDanger: this.props.smallDogDanger,
+      dominant: this.props.dominant,
+      doesntShare: this.props.doesntShare,
+      chaser: this.props.chaser,
+      wrestler: this.props.wrestler,
+      allDogFriendly: this.props.allDogFriendly,
+      editActive: false
+    };
+  }
 
-function ProfileCard(props) {
+  handleEdit = event => {
+    event.preventDefault();
+    this.setState({
+      editActive: true
+    });
+  };
+  // need to set editActive to false on click of savebtn, also need api call (update route)
 
-    console.log(props)
+  render() {
     return (
-        <>
+      <>
         <div className="col-md-12">
-            <div className="card text-white bg-info mb-3">
-                <div className="card-header">
-                    <h4 style={h4Style}>{props.dogName}'s Profile
-                        
-                    </h4>  
-                    <EditRemoveBtn 
-                            _id={props._id}
-                            loadPuppers={props.loadPuppers}
-                        /> 
-                </div>
-                <div className="card-body row">
-                    <ProfileImage {...props} />
-                    <ProfileSummary {...props} />
-                    {/* <ProfileEdit {...props} /> */}
-                </div>
+          <div className="card text-white bg-info mb-3">
+            <div className="card-header">
+              <h4 style={h4Style}>{this.props.dogName}'s Profile</h4>
+              <EditRemoveBtn
+                _id={this.props._id}
+                loadPuppers={this.props.loadPuppers}
+                handleEdit={this.handleEdit}
+              />
             </div>
+            <div className="card-body row">
+              <ProfileImage {...this.props} />
+              {this.state.editActive ? (
+                <ProfileEdit
+                  {...this.props}
+                  pupper={this.state}
+                />
+              ) : (
+                <ProfileSummary {...this.props} />
+              )}
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
+  }
 }
 
 export default ProfileCard;
--- client/src/components/ProfileEdit.js
+++ client/src/components/ProfileEdit.js
@@ -5,10 +5,12 @@ import SizeMenu from "./SizeMenu";
 
 
 class ProfileEdit extends Component {  
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {
         myPupper: this.props.pupper,
     }
-
+    }
 componentDidMount() {
     console.log(this.state.myPupper)
 };
@@ -37,31 +39,147 @@ render() {
             <div className="row">
                 <div className="col-md-6 col-sm-12">
                 <br/>
-                    Size:&nbsp;&nbsp;<select className="form-control-sm form-group"><SizeMenu value={this.props.size}/></select>
+                    Size:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="size"
+                  value={this.state.myPupper.size}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="small">
+                    Small (under 25 lbs)
+                  </option>
+                  <option value="medium">
+                    Medium (between 25-50 lbs)
+                  </option>
+                  <option value="large">
+                    Large (over 50 lbs)
+                  </option>
+                  </select>
                     <br/>
-                    Family friendly:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.familyFriendly}/></select> 
+                    Family friendly:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="familyFriendly"
+                  value={this.state.myPupperfamilyFriendly}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select> 
                     <br/>
-                    Energetic:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.energetic}/></select>
+                    Energetic:&nbsp;&nbsp;<select
+                  className="form-control form-group col-md-6 questions"
+                  name="energetic"
+                  value={this.state.myPupper.energetic}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                     <br/>
-                    Skittish (dogs):&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.dogDanger}/></select>
+                    Skittish (dogs):&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="dogDanger"
+                  value={this.state.myPupper.dogDanger}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                     <br/>
-                    Skittish (people):&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.strangerDanger}/></select>
+                    Skittish (people):&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="strangerDanger"
+                  value={this.state.myPupper.strangerDanger}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                     <br/>
-                    Large playmates:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.largeDogDanger}/></select>
+                    Large playmates:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="largeDogDanger"
+                  value={this.state.myPupper.largeDogDanger}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 </div>
                 <div className="col-md-6 col-sm-12">
                 <br/>
-                    Small playmates:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.smallDogDanger}/></select> 
+                    Small playmates:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="smallDogDanger"
+                  value={this.state.myPupper.smallDogDanger}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select> 
                     <br/>
-                    Dominant:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.dominant}/></select> 
+                    Dominant:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="dominant"
+                  value={this.state.myPupper.dominant}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select> 
                     <br/>
-                    Guards resouces:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.doesntShare}/></select>
+                    Guards resouces:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="doesntShare"
+                  value={this.state.myPupper.doesntShare}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                     <br/>
-                    Loves chase:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.chaser}/></select>
+                    Loves chase:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  name="chaser"
+                  value={this.state.myPupper.chaser}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                     <br/>
-                    Loves wrestling:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.wrestler}/></select> 
+                    Loves wrestling:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  id="q12"
+                  name="wrestler"
+                  value={this.state.myPupper.wrestler}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select> 
                     <br/>
-                    Outgoing:&nbsp;&nbsp;<select className="form-control-sm form-group"><YesNoMenu value={this.props.allDogFriendly}/></select>
+                    Outgoing:&nbsp;&nbsp;
+                    <select
+                  className="form-control form-group col-md-6 questions"
+                  id="q13"
+                  name="allDogFriendly"
+                  value={this.state.myPupper.allDogFriendly}
+                  onChange={this.handleInputChange}
+                >
+                  <option value="yes">Yes</option>
+                  <option value="no">No</option>
+                </select>
                 </div>
             </div>
             <div className="row">
